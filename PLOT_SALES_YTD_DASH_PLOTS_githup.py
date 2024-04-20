65ec73846cb4bc68fbd969bf98b13067187982e6
Drwaleed2022 <120225963+Drwaleed2022@users.noreply.github.com>
2024-04-20 19:40:53 +0200

    Update PLOT_SALES_YTD_DASH_PLOTS_githup.py

--- PLOT_SALES_YTD_DASH_PLOTS_githup.py
+++ PLOT_SALES_YTD_DASH_PLOTS_githup.py
@@ -67,7 +67,7 @@ def update_output(sales_rep,month):
     #webbrowser.open_new("http://localhost:{}".format(port))                 
 if __name__ == '__main__':
     #Timer(1,open_browser).start();
-    app.run_server(debug=False)
+    app.run_server(port=8050,debug=False)
  
 
    
