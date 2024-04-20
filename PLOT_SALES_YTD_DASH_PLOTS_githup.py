190d9d4c1f6ef64c94d238ba2ae46f4111b6e2a7
Drwaleed2022 <120225963+Drwaleed2022@users.noreply.github.com>
2024-04-20 23:25:51 +0200

    Update PLOT_SALES_YTD_DASH_PLOTS_githup.py

--- PLOT_SALES_YTD_DASH_PLOTS_githup.py
+++ PLOT_SALES_YTD_DASH_PLOTS_githup.py
@@ -65,24 +65,10 @@ def update_output(sales_rep,month):
 #port=8070
 #def open_browser() :
     #webbrowser.open_new("http://localhost:{}".format(port))  
-import os
 
-
-
-workers = int(os.environ.get('GUNICORN_PROCESSES', '2'))
-
-threads = int(os.environ.get('GUNICORN_THREADS', '4'))
-
-# timeout = int(os.environ.get('GUNICORN_TIMEOUT', '120'))
-
-bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
-
-forwarded_allow_ips = '*'
-
-secure_scheme_headers = { 'X-Forwarded-Proto': 'https' }
 if __name__ == '__main__':
     #Timer(1,open_browser).start();
-    app.run_server(host='0.0.0.0',port=8080,debug=False)
+    app.run_server(host='54.254.162.138',port=8080,debug=False)
  
 
    
