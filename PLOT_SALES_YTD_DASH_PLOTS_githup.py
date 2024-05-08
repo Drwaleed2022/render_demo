3f747df20ca4a359e895339c52a92e205cac007e
Drwaleed2022 <120225963+Drwaleed2022@users.noreply.github.com>
2024-05-09 00:36:34 +0300

    Update PLOT_SALES_YTD_DASH_PLOTS_githup.py

--- PLOT_SALES_YTD_DASH_PLOTS_githup.py
+++ PLOT_SALES_YTD_DASH_PLOTS_githup.py
@@ -95,7 +95,7 @@ def update_output(area,date,pharmacy):
 
 if __name__ == '__main__':
     #Timer(1,open_browser).start();
-    app.run_server(host='0.0.0.0',port=5000,debug=False)
+    app.run_server(host='0.0.0.0',port=5050,debug=False)
  
 
    
