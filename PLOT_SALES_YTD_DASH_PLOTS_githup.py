9a4282f071cd8f7c6cc10a90add8b25c8d2d05db
Drwaleed2022 <120225963+Drwaleed2022@users.noreply.github.com>
2024-05-08 23:54:32 +0300

    Update PLOT_SALES_YTD_DASH_PLOTS_githup.py

--- PLOT_SALES_YTD_DASH_PLOTS_githup.py
+++ PLOT_SALES_YTD_DASH_PLOTS_githup.py
@@ -1,81 +1,71 @@
 import pandas as pd
 import plotly.express as px
-from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import dash
 from dash import html,dcc,dash_table
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ydata_profiling import ProfileReport
+from arabic_reshaper import arabic_reshaper
 import calendar
 import datetime
 import gunicorn
-#
-Area_rep_sales=pd.read_csv('https://raw.githubusercontent.com/Drwaleed2022/render_demo/main/Area_rep_sales.csv')
-Area_rep_sales.set_index('Area_rep',inplace=True)
-Area_sales=pd.read_csv('https://raw.githubusercontent.com/Drwaleed2022/render_demo/main/Area_sales.csv')
-Area_sales.set_index('Area',inplace=True)
-sales_rep_sales=pd.read_csv('https://raw.githubusercontent.com/Drwaleed2022/render_demo/main/sales_rep_sales.csv')
-sales_rep_sales.set_index('sales_rep',inplace=True)
-Sales_representative=['ESRAA','SHIMAA','AYA','OMET','ISUPPLY','ISLAM','TOTAL']
-for i in Sales_representative:
-    sales_rep= i
-    print(sales_rep)
-ALL_YEAR= ['JAN','FEB','MAR','APR','MAY','JUN','JUL','AUG','SEP','OCT','NOV','DEC','YTD']
-for i in ALL_YEAR:
-    month=i
-    print(month)  
-AREA=['GOMLA', 'Alexandria', 'Behira', 'Matrouh','Total_pharmacy','Global_sales']
+import webbrowser
+from threading import Timer
+AREA_REP_SALES_PROFIT=pd.read_csv("E:/DASHBOARD/AREA_REP_SALES_PROFIT.csv")
+AREA_REP_SALES=pd.read_csv("E:/DASHBOARD/AREA_REP_SALES.csv")
+AREA_REP_SALES_reindex=pd.read_csv("E:/DASHBOARD/AREA_REP_SALES_reindex.csv")
+all_pharmacies_sales=pd.read_csv("E:/DASHBOARD/all_pharmacies_sales.csv")
+AREA_REP_SALES.set_index('Area_rep',inplace=True)
+AREA=AREA_REP_SALES.index
 for i in AREA:
     area=i
     print(area)
-AREA_REP=['GOMLA / ISLAM','Alexandria / ESRAA','Behira / SHIMAA','Alexandria / SHIMAA','Alexandria / OMET','Matrouh / SHIMAA','Alexandria / AYA','Alexandria / ISUPPLY','Behira / AYA','Alexandria / others','Behira / OMET']
-for i in AREA_REP:
-    area_rep=i
-    print(area_rep)    
-
-# Create a dash application
+Date=AREA_REP_SALES.columns
+for i in Date:
+    date=i
+    print(date)
+all_pharmacies_sales.set_index('pharmacy',inplace=True)   
+PHARMACY=all_pharmacies_sales.index
+for i in PHARMACY:
+    pharmacy= i
+    print(pharmacy)     
 from dash import Output
-from dash import Dash
 from dash.dash import Input
-      
-import flask, multiprocessing
-app = Dash(__name__, suppress_callback_exceptions=True)
-server=app.server
+app = dash.Dash(__name__)
 # Build dash app layout
 app.layout = html.Div(children=[ html.H1('2024 Sales Statistics Dashboard', 
                                 style={'textAlign': 'center', 'color': '#503D36',
                                 'font-size': 35}),
                                 
-                                html.Div([html.Div(["Sales rep:",dcc.Dropdown(Sales_representative,id='sales_rep', value='SHIMAA', 
-                                style={'height':'35px','width':'500px', 'font-size': 20})]),
-                                html.Div(["Area:",dcc.Dropdown(AREA,id='area', value='Global_sales',                               
-                                style={'height':'35px','width':'500px', 'font-size': 20})]),  
+                                html.Div([
+                                        html.Div(["Date:"  ,dcc.Dropdown(Date,id='date',value='5/2024', 
+                                style={'height':'35px','width':'700px', 'font-size': 20})]),
+                                        html.Div(["AREA:",dcc.Dropdown(AREA,id='area', value='global_sales', 
+                                style={'height':'35px','width':'700px', 'font-size': 20})]),
+                                        html.Div(["Pharmacy:"  ,dcc.Dropdown(PHARMACY,id='pharmacy',value='ص المعسكر الروماني/شهري',
+                                style={'height':'35px','width':'700px', 'font-size': 20})]),  
                                                                                                     
-                                ],style={'font-size': 25,'display': 'flex'}),
-                                html.Div([
-                                        
-                                        html.Div(["Month:"  ,dcc.Dropdown(ALL_YEAR,id='month',value='APR', 
-                                style={'height':'35px','width':'500px', 'font-size': 20})]),
-                                html.Div(["Area_rep:"  ,dcc.Dropdown(AREA_REP,id='area_rep',value='Alexandria / SHIMAA', 
-                                style={'height':'35px','width':'500px', 'font-size': 20})])  
-                                                            
-                                ],style={'font-size': 25,'display': 'flex'}),                               
-                                html.Br(),
-                                html.Br(), 
-                                  # Segment 1
-                                html.Div([
-                                        html.Div(dcc.Graph(id='area_curve1')),
-                                        html.Div(dcc.Graph(id='area_curve2'))
-                                ], style={'font-size': 25,'display': 'flex'}),                            
+                                ],style={'font-size': 25,'display': 'flex'}),                                                             
+                               
+                                                                                    
+                                # Segment 1
+                                html.Div([                                        
+                                        html.Div(dcc.Graph(id='area_rep_curve0')),
+                                        html.Div(dcc.Graph(id='area_rep_curve1')),
+                                         html.Div(dcc.Graph(id='all_pharmacies_curve'))
+                                ], style={'font-size': 25,'display': 'flex'}),
                                 # Segment 2
                                 html.Div([                                        
-                                        html.Div(dcc.Graph(id='sales_rep_curve1')),
-                                        html.Div(dcc.Graph(id='sales_rep_curve2'))
+                                        html.Div(dcc.Graph(id='area_rep_curve2')),
+                                        html.Div(dcc.Graph(id='area_rep_curve3')),
+                                        html.Div(dcc.Graph(id='area_rep_curve4'))
                                 ], style={'font-size': 25,'display': 'flex'}),
-                                 # Segment 3
-                                html.Div([                                        
-                                        html.Div(dcc.Graph(id='area_rep_curve1')),
-                                        html.Div(dcc.Graph(id='area_rep_curve2'))
-                                ], style={'font-size': 25,'display': 'flex'})
+                                # Segment 3
+                                html.Div([
+                                        html.Div( dash_table.DataTable(AREA_REP_SALES_reindex.to_dict('records'), [{"name": i, "id": i} for i in AREA_REP_SALES_reindex.columns]))
+                                ], style={'font-size': 20,'color': 'blue'}), 
                                 
                                 ])
 
@@ -85,26 +75,25 @@ app.layout = html.Div(children=[ html.H1('2024 Sales Statistics Dashboard',
     return df_X'''
 # Callback decorator
 @app.callback( [
-               Output(component_id='area_curve1', component_property='figure'),
-               Output(component_id='area_curve2', component_property='figure'),
-               Output(component_id='sales_rep_curve1', component_property='figure'),
-               Output(component_id='sales_rep_curve2', component_property='figure'),
+               Output(component_id='area_rep_curve0', component_property='figure'),
                Output(component_id='area_rep_curve1', component_property='figure'),
-               Output(component_id='area_rep_curve2', component_property='figure')
+               Output(component_id='all_pharmacies_curve', component_property='figure'),
+               Output(component_id='area_rep_curve2', component_property='figure'),
+               Output(component_id='area_rep_curve3', component_property='figure'),
+               Output(component_id='area_rep_curve4', component_property='figure')
             ],
                [Input(component_id='area', component_property='value'),
-                Input(component_id='sales_rep', component_property='value'),
-                Input(component_id='month', component_property='value')],
-                Input(component_id='area_rep', component_property='value'))
+                Input(component_id='date', component_property='value'),
+                Input(component_id='pharmacy', component_property='value')])
 # Computation to callback function and return graph
-def update_output(area,sales_rep,month,area_rep): 
-    area_curve1=px.bar(Area_sales,y=month,color=month,title =  'Sales per Area in selected month',text_auto=True)
-    area_curve2=px.bar(Area_sales.loc[area],color=ALL_YEAR,title =  'Sales in selected Area',text_auto=True)
-    sales_rep_curve1=px.bar(sales_rep_sales,y=month,color=month,title = 'Sales per rep in selected month',text_auto=True)
-    sales_rep_curve2=px.bar(sales_rep_sales.loc[sales_rep],color=ALL_YEAR,title = 'Sales  in 2024 by specific sales rep',text_auto=True)
-    area_rep_curve1=px.bar(Area_rep_sales,y=month,color=month,text_auto=True,title= 'Sales of rep per area in selected month')
-    area_rep_curve2=px.bar(Area_rep_sales.loc[area_rep],color=ALL_YEAR,text_auto=True,title= 'Sales of specific rep in specific area')
-    return[area_curve1,area_curve2,sales_rep_curve1,sales_rep_curve2,area_rep_curve1,area_rep_curve2]
+def update_output(area,date,pharmacy): 
+    area_rep_curve0=px.bar(AREA_REP_SALES,y=date,color=AREA,text_auto=True,title= 'Whole sales per Area & rep in specific date')
+    area_rep_curve1=px.bar(AREA_REP_SALES.loc[area],color=Date,text_auto=True,title= 'Whole sales per Area & rep')
+    all_pharmacies_curve=px.bar(all_pharmacies_sales.loc[pharmacy],color=Date,orientation='h',text_auto=True,title='Sales of pharmacy in 2024')
+    area_rep_curve2=px.scatter(AREA_REP_SALES.loc[['ALEXANDRIA_GLOBAL','BEHIRA_GLOBAL','MATROUH_GLOBAL']], y=date, size=date,text=date,hover_name=date, title='Geographical sales in specific month',color=date, size_max=120)
+    area_rep_curve3=px.scatter(AREA_REP_SALES.loc[['SHIMAA_GLOBAL','ESRAA_GLOBAL','AYA_GLOBAL','OMET_GLOBAL','I_SUPPLY_GLOBAL']], y=date, size=date,text=date,hover_name=date, title='Sales rep sales in specific month',color=date, size_max=120)
+    area_rep_curve4=px.pie(AREA_REP_SALES.loc[[ 'GOMLA / ISLAM', 'global_pharmacy']],names=['GOMLA / ISLAM', 'global_pharmacy'],values=date,title = ' Gomla & Pharmacy sales ratio in specific month ' )
+    return[area_rep_curve0,area_rep_curve1,all_pharmacies_curve,area_rep_curve2,area_rep_curve3,area_rep_curve4]
 #port=8070
 #def open_browser() :
     #webbrowser.open_new("http://localhost:{}".format(port))  
