3664fff2c2babd5e93087f45bfe77a9e7c19425f
Drwaleed2022 <120225963+Drwaleed2022@users.noreply.github.com>
2025-01-03 20:27:10 +0200

    Update PLOT_SALES_YTD_DASH_PLOTS_githup.py

--- PLOT_SALES_YTD_DASH_PLOTS_githup.py
+++ PLOT_SALES_YTD_DASH_PLOTS_githup.py
@@ -5,8 +5,6 @@ import plotly.graph_objects as go
 import dash
 from dash import html,dcc,dash_table
 import numpy as np
-from ydata_profiling import ProfileReport
-from arabic_reshaper import arabic_reshaper
 import calendar
 import datetime
 import webbrowser
